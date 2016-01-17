6b17c8c7f3966d0f79c7504472ff6f5e9020b7db
Brian Baskin <brian@thebaskins.com>
2016-01-17 16:44:32 -0500

    Small update
    Added variables

--- NoribenSandbox.bat
+++ NoribenSandbox.bat
@@ -8,21 +8,28 @@
 @echo off
 if "%1"=="" goto HELP
 if not exist "%1" goto HELP
-SET CWD=%CD%
-SET VMRUN="C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe"
-SET VMX="e:\VMs\WinXP_Malware\WinXP_Malware.vmx"
-SET VM_SNAPSHOT="Baseline"
+
+set DELAY=10
+set CWD=%CD%
+set VMRUN="C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe"
+set VMX="e:\VMs\WinXP_Malware\WinXP_Malware.vmx"
+set VM_SNAPSHOT="Baseline"
 SET VM_USER=Administrator
 set VM_PASS=password
-set DELAY=10
 set FILENAME=%~nx1
+set NORIBEN_PATH="C:\Documents and Settings\%VM_USER%\Desktop\Noriben.py"
+set LOG_PATH="C:\Noriben_Logs"
+set ZIP_PATH="C:\Tools\zip.exe"
+
+
 
 %VMRUN% -T ws revertToSnapshot %VMX% %VM_SNAPSHOT%
 %VMRUN% -T ws start %VMX%
 %VMRUN% -gu %VM_USER%  -gp %VM_PASS% copyFileFromHostToGuest %VMX% "%1" C:\Malware\malware.exe
-%VMRUN% -T ws -gu %VM_USER% -gp %VM_PASS% runProgramInGuest %VMX% C:\Python27\Python.exe "C:\Documents and Settings\%VM_USER%\Desktop\Noriben.py" -d -t %DELAY% --cmd "C:\Malware\Malware.exe" --output "C:\Noriben_Logs"
+echo %VMRUN% -T ws -gu %VM_USER% -gp %VM_PASS% runProgramInGuest %VMX% C:\Python27\Python.exe %NORIBEN_PATH% -d -t %DELAY% --cmd "C:\Malware\Malware.exe" --output %LOG_PATH%
+%VMRUN% -T ws -gu %VM_USER% -gp %VM_PASS% runProgramInGuest %VMX% C:\Python27\Python.exe %NORIBEN_PATH% -d -t %DELAY% --cmd "C:\Malware\Malware.exe" --output %LOG_PATH%
 if %ERRORLEVEL%==1 goto ERROR1
-%VMRUN% -T ws -gu %VM_USER% -gp %VM_PASS% runProgramInGuest %VMX% C:\Tools\zip.exe -j C:\NoribenReports.zip C:\Noriben_Logs\*.*
+%VMRUN% -T ws -gu %VM_USER% -gp %VM_PASS% runProgramInGuest %VMX% %ZIP_PATH% -j C:\NoribenReports.zip %LOG_PATH%\*.*
 %VMRUN% -gu %VM_USER%  -gp %VM_PASS% copyFileFromGuestToHost %VMX% C:\NoribenReports.zip %CWD%\NoribenReports_%FILENAME%.zip
 goto END
 
